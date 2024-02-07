55d2793767a7da0210e18f68378f0fde8c401c78
Aliaksei Bely <Aliaksei.Bely@solbeg.com>
2024-02-07 18:20:25 +0100

    replace

---
+++ 6_Reflection/DIContainer/Attributes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIContainer
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+	public abstract class ContainerAttribute : Attribute
+	{
+	}
+
+	[AttributeUsage(AttributeTargets.Property)]
+	public class ImportAttribute : ContainerAttribute
+	{
+	}
+
+	[AttributeUsage(AttributeTargets.Class)]
+	public class ImportConstructorAttribute : ContainerAttribute
+	{
+	}
+
+	[AttributeUsage(AttributeTargets.Class)]
+	public class ExportAttribute : ContainerAttribute
+	{
+		public Type Contract { get; private set; }
+
+		public ExportAttribute(Type contract = null)
+		{
+			Contract = contract;
+		}
+	}
+}
